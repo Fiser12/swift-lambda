e18c4bd19805dd775c572f684bee4dcf784c1d33
rgarciah <ruben.garcia@tyris-software.com>
2024-02-10 15:46:29 +0100

    feat: Added basic arrows operators, testing and docs

--- Sources/swift-lambda/Operators/Arrows.swift
+++ Sources/swift-lambda/Operators/Arrows.swift
@@ -3,35 +3,63 @@
 //
 //
 //  Created by Rubén García on 10/2/24.
-//
+//  Idea got from pointfree episode 1: https://www.pointfree.co/episodes/ep1-functions
 
-import Foundation
-
+/** Define the precedence for forward application of functions.
+ This group allows for left-to-left chaining of the pipe forward operator, ensuring that operations are applied from left to right as they appear in the code.
+ */
 precedencegroup ForwardApplication {
   associativity: left
 }
 
+/** Define the precedence for forward composition of functions.
+ This group ensures that function compositions are evaluated with a higher precedence than forward application, allowing for seamless chaining of composed functions.
+ */
 precedencegroup ForwardComposition {
   associativity: left
   higherThan: ForwardApplication
 }
 
+/**
+ Defines a precedence group for applying a function to a value with right associativity. This group allows for a right-to-left evaluation order, which can be useful in certain contexts to reverse the natural flow of function application, making the code more flexible and expressive.
+ */
 precedencegroup ForwardApplicationPrecedence {
     associativity: right
 }
 
+/**
+ Defines a precedence group for composing functions with right associativity. Ensuring that compositions are evaluated in a right-to-left order when multiple composition operators are used together. This precedence is essential for constructing intuitive and readable chains of function compositions that behave as expected without explicit parentheses.
+ */
 precedencegroup ForwardCompositionPrecedence {
     associativity: right
     higherThan: ForwardApplicationPrecedence
 }
 
 
+/** Apply a function to a value.
+ This "pipe-forward" operator allows for a more readable, left-to-right syntax when applying functions to values, mirroring the flow of data.
+ Inspired by similar operators in languages like F#, Elixir, and Elm, it aims to enhance readability and expressiveness in Swift.
+ 
+ - Parameters:
+   - a: The value to apply the function to.
+   - f: The function to apply to the value.
+ - Returns: The result of applying the function to the given value.
+ */
 infix operator |>: ForwardApplication
 
 public func |> <A, B>(a: A, f: (A) -> B) -> B {
   return f(a)
 }
 
+/** Composes two functions from left to right.
+ This "forward compose" or "right arrow" operator enables the chaining of two functions where the output of the first is the input of the second.
+ It represents a fundamental concept in functional programming, allowing for the creation of more complex functions from simpler ones without the need for nesting.
+ 
+ - Parameters:
+   - f: The first function to apply.
+   - g: The second function to apply, which takes the output of the first function as its input.
+ - Returns: A new function that represents the composition of `f` and `g`.
+ */
 infix operator >>>: ForwardComposition
 
 public func >>> <A, B, C>(f: @escaping (A) -> B, g: @escaping (B) -> C) -> ((A) -> C) {
@@ -40,16 +68,32 @@ public func >>> <A, B, C>(f: @escaping (A) -> B, g: @escaping (B) -> C) -> ((A) 
   }
 }
 
+/** Apply a function to a value, with right associativity.
+ This operator is similar to `|>`, but with right associativity, allowing for a different grouping of operations without altering the fundamental behavior.
+ 
+ - Parameters:
+   - f: The function to apply to the value.
+   - a: The value to apply the function to.
+ - Returns: The result of applying the function to the given value.
+ */
 infix operator <|: ForwardApplicationPrecedence
 
 public func <| <A, B>(f: (A) -> B, a: A) -> B {
   return f(a)
 }
 
+/** Composes two functions from right to left.
+ Opposite to the `>>>` operator, this "backward compose" allows for composing functions in reverse order, which can be useful in certain contexts to improve readability or align with specific logic.
+ 
+ - Parameters:
+   - f: The second function to apply.
+   - g: The first function to apply, which takes the input and whose output is passed to `f`.
+ - Returns: A new function that represents the composition of `g` followed by `f`.
+ */
 infix operator <<<: ForwardCompositionPrecedence
 
 public func <<< <A, B, C>(f: @escaping (B) -> C, g: @escaping (A) -> B) -> ((A) -> C) {
-  return { a in 
+  return { a in
       f(g(a))
   }
 }
--- Tests/swift-lambdaTests/Operators/ArrowsTests.swift
+++ Tests/swift-lambdaTests/Operators/ArrowsTests.swift
@@ -10,31 +10,47 @@ import XCTest
 import SwiftLambda
 
 final class ArrowsTests: XCTestCase {
-    func testSimpleArrow() throws {
-        let operate = 2 |> incr |> square
-        XCTAssertTrue(operate == 9)
+    // Tests the basic functionality of the pipe forward operator with simple functions
+    func testPipeForwardOperatorWithSimpleFunctions() throws {
+        let result = 2 |> incr |> square
+        XCTAssertEqual(result, 9, "The result of incrementing 2 and then squaring should be 9.")
     }
     
-    func testComplexArrow() throws {
-        let operate = 2 |> incr >>> square
-        XCTAssertTrue(operate == 9)
+    // Tests the forward composition operator combining two functions into one
+    func testForwardCompositionOperator() throws {
+        let composedFunction = incr >>> square
+        let result = composedFunction(2)
+        XCTAssertEqual(result, 9, "The result of composing incr and square functions and applying to 2 should be 9.")
+    }
+    
+    // Tests the application of functions in reverse order to verify the precedence of pipe backward operator
+    func testPipeBackwardOperatorWithSimpleFunctions() throws {
+        let result = square <| incr <| 2
+        XCTAssertEqual(result, 9, "The result of incrementing 2 and then squaring, using the backward pipe, should be 9.")
+    }
+    
+    // Tests the backward composition of functions to ensure correct function composition and application
+    func testBackwardCompositionOperator() throws {
+        let composedFunction = square <<< incr
+        let result = composedFunction <| 2
+        XCTAssertEqual(result, 9, "The result of composing incr and square functions in reverse and applying to 2 should be 9.")
+    }
+    
+    func testReversibilityOfComposition() throws {
+        let forwardComposition = incr >>> square
+        let backwardComposition = square <<< incr
+        let forwardResult = forwardComposition(2)
+        let backwardResult = backwardComposition(2)
+        XCTAssertEqual(forwardResult, backwardResult, "Forward and backward compositions of non-commutative functions should not produce the same result.")
+        XCTAssertEqual(forwardResult, 9, "Both should result in 9.")
     }
 
-    func testSimpleArrowPrecedence() throws {
-        let operate = square <| incr <| 2
-        XCTAssertTrue(operate == 9)
-    }
-    
-    func testComplexArrowPrecedence() throws {
-        let operate = square <<< incr <| 2
-        XCTAssertTrue(operate == 9)
-    }
 }
 
 func incr(_ x: Int) -> Int {
-  return x + 1
+    return x + 1
 }
 
 func square(_ x: Int) -> Int {
-  return x * x
+    return x * x
 }
